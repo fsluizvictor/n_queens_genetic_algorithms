05a25f2524a000e2056dca6a2e742badc4234865
fsluizvictor <fs.luizvictor@gmail.com>
2021-06-24 00:56:15 -0300

    make the algoorithm ga

--- src/models/n_queens.py
+++ src/models/n_queens.py
@@ -7,7 +7,7 @@ from src.interfaces.individual import Individual
 
 class NQueens(object):
 
-    def __init__(self, amount_queens: Optional[int] = 8, genes: Optional[List[int]] = None):
+    def __init__(self, amount_queens: Optional[int] = 8, genes: Optional[List[int]] = None, rate: Optional[int] = 0):
         # inicializar o tamanho do vetor de genes e seus valores de forma aleatória
         # os valores aleatórios devem obedecer a ideia da permutação
         self._amount_queens = amount_queens
@@ -15,7 +15,9 @@ class NQueens(object):
             self._genes = genes
         else:
             self._genes = [i for i in range(self._amount_queens)]
-            shuffle(self._genes)
+        shuffle(self._genes)
+        self.rate = rate
+        self.rated = False
 
     @property
     def amount_queens(self):
@@ -32,3 +34,19 @@ class NQueens(object):
     @genes.setter
     def genes(self, genes: List[int]):
         self._genes = genes
+
+    @property
+    def rate(self):
+        return self.rate
+
+    @rate.setter
+    def rate(self, rate: int):
+        self.rate = rate
+
+    @property
+    def rated(self):
+        return self.rated
+
+    @rated.setter
+    def rated(self, rated: bool):
+        self.rated = rated
--- src/services/genetic_algorithm.py
+++ src/services/genetic_algorithm.py
@@ -1,14 +1,26 @@
 from typing import List
 
 from src.interfaces.individual import Individual
+from src.models.n_queens import NQueens
+from src.services.n_queens_service import NQueensService
 
 
 class GeneticAlgorithm:
 
-    def execute(self, amount_steps: int, amount_individuals: int) -> Individual:
+    def __init__(self, service: NQueensService):
+        self.service = service
+
+    def execute(self, amount_steps: int, amount_individuals: int) -> List[NQueens]:
         # fazer um for para preencher uma lista popIni com o nInd(individuos)
         # para gerar os incdividuos iniciar utilizar o individuo factory passado por parametro
-        pass
+        initial_population = list()
+        for i in range(amount_individuals):
+            initial_population.append(NQueens())
+
+        sons_population = self.generate_sons(initial_population)
+        mutants_population = self.generate_mutants(initial_population)
+
+        return list()
 
     def show_better_individual(self, generation: int, individual_population: List[Individual]):
         # imprimir o numero da geração, o melhor individuo e a avaliação deste melhor individuo
@@ -16,7 +28,7 @@ class GeneticAlgorithm:
         pass
 
     def selection(self, individual_population: List[Individual], amount_individual, amount_elitism: int) -> List[
-        Individual]:
+        NQueens]:
         # ELITISMO
         # verificar se o problema é de minimização, caso seja é preciso ordenar o array do menor para maior avaliação
         # selecionar os "amount_elitism" individuos no inicio do array e coloca-los em uma nova população
@@ -34,8 +46,20 @@ class GeneticAlgorithm:
         # observação --> realizar os passos 1,2,3 e 4 n vezes até completar newPop.size() = amount_individual
         pass
 
-    def find_sons(self, individual_population: List[Individual]) -> List[Individual]:
-        pass
+    def generate_sons(self, individual_population: List[NQueens]) -> List[NQueens]:
+        sons = list()
+        for i in range(0, len(individual_population) - 2, 2):
+            for j in range(1, len(individual_population) - 1, 2):
+                list_sons = self.service.recombine(individual_population[i], individual_population[j])
+                sons.append(list_sons[0])
+                sons.append(list_sons[1])
 
-    def find_mutantes(self, individual_population: List[Individual]) -> List[Individual]:
-        pass
+        return sons
+
+    def generate_mutants(self, individual_population: List[NQueens]) -> List[NQueens]:
+        mutants = list()
+        for i in range(len(individual_population)):
+            mutant = self.service.mutate(individual_population[i])
+            mutants.append(mutant)
+
+        return mutants
